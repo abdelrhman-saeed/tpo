84fa013bb89557202db268cbaaabad77e2f4b356
abdelrhman-saeed <abdelrhmansaeed001@gmail.com>
2025-01-22 16:59:20 +0200

    fix: fixed hotel search route issue

--- src/Views/hotelSearch.php
+++ src/Views/hotelSearch.php
@@ -6,91 +6,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hotel Search</title>
-<<<<<<< HEAD
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-=======
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 20px;
-        }
-
-        .container {
-            /* max-width: 600px; */
-            margin: 0 auto;
-        }
-
-        .rooms_container {
-            border: 2px solid black;
-            padding: 2em;
-            width: 100%;
-            margin-bottom: 2em;
-            box-sizing: border-box;
-        }
-
-        .room {
-            border: 1px solid black;
-            padding: 1em;
-            margin-top: 1em;
-            box-sizing: border-box;
-        }
-
-        label {
-            display: block;
-            margin-bottom: 0.5em;
-        }
-
-        input[type="number"],
-        input[type="date"],
-        input[type="text"] {
-            width: 100%;
-            padding: 0.5em;
-            margin-bottom: 1em;
-            box-sizing: border-box;
-        }
-
-        button {
-            padding: 0.5em 1em;
-            background-color: #007BFF;
-            color: white;
-            border: none;
-            cursor: pointer;
-        }
-
-        button:hover {
-            background-color: #0056b3;
-        }
-
-        input[type="submit"] {
-            padding: 0.5em 1em;
-            background-color: #28a745;
-            color: white;
-            border: none;
-            cursor: pointer;
-        }
-
-        input[type="submit"]:hover {
-            background-color: #218838;
-        }
-
-        .error {
-            color: red;
-            margin-bottom: 1em;
-        }
-    </style>
->>>>>>> 75bec5944b1a50eab2c42cf4706da5bdd44d3a52
 </head>
 
 
 <body>
     <div class="container mt-5">
-        <form action="/TBOHolidays_HotelAPI/AvailableHotelRooms" method="post" class="mb-3">
+        <form action="/TBOHolidays_HotelAPI/HotelSearch" method="post" class="mb-3">
             <div class="form-group">
                 <label for="checkIn">Check-In Date</label>
                 <input type="date" class="form-control" id="checkIn" name="CheckIn" required>
@@ -173,8 +95,6 @@
                     `);
                 }
             });
-<<<<<<< HEAD
-=======
 
             $("#hotelSearchForm").submit(function(event) {
                 let isValid = true;
@@ -211,7 +131,6 @@
                     event.preventDefault();
                 }
             });
->>>>>>> 75bec5944b1a50eab2c42cf4706da5bdd44d3a52
         });
     </script>
 
